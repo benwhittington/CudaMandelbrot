4afd0665b098b6761f36fe781a11bd57e23784c5
benwhittington <benjiwhittington@gmail.com>
2021-08-28 15:16:33 +1200

    write to cerr

--- src/cudaHelpers.cuh
+++ src/cudaHelpers.cuh
@@ -5,7 +5,7 @@
 
 #include <iostream>
 
-#define CUDA_REQUIRE_SUCCESS(expr) { cuda_check((expr),__FILE__,__func__,#expr,__LINE__); }
+#define CUDA_REQUIRE_SUCCESS(expr) { cuda_check((expr),__FILE__,__PRETTY_FUNCTION__,#expr,__LINE__); }
 #define cuda_peek_last_error() { CUDA_REQUIRE_SUCCESS(cudaPeekAtLastError()); }
 #define cuda_sync(){ CUDA_REQUIRE_SUCCESS(cudaDeviceSynchronize()); }
 #define cuda_malloc(devPtr, size) { CUDA_REQUIRE_SUCCESS(cudaMalloc(devPtr, size)); }
@@ -15,10 +15,9 @@
 // shamelessly stolen from David (github.com/DCGroothuizenDijkema/), thank you David.
 inline void cuda_check(const cudaError_t code, const char * const file, const char * const func, const char * const call, const int line) {
     if (code != cudaSuccess) {
-        std::cout
+        std::cerr
         << file << ":" << line << ": CUDA ERROR (" << code << "): " << cudaGetErrorName(code) << ": " << cudaGetErrorString(code) << '\n'
-        << "  " << func << "()\n"
-        << "  {\n"
+        << "  " << func << "() {\n"
         << "    " << call << '\n'
         << "  }" << std::endl;
         exit(code);
